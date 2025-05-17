0db32ab763bb01ad4ccb483ee238b6bfc9bcedb8
Chrisssssstalee <toledo.christalee24@gmail.com>
2025-05-17 02:29:25 -0700

    Update title

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -11,7 +11,7 @@ export default function Navbar() {
         <div className="flex justify-between h-16">
           <div className="flex items-center">
             <Link href="/" className="text-xl font-bold text-gray-800 hover:text-gray-600">
-              Your Name
+              Christalee Toledo
             </Link>
           </div>
           
